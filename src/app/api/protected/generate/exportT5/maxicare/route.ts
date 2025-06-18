33deea34921a6987a73f9750a0acf30ff4dcc347
raizelybaam <95033305+raizelycid@users.noreply.github.com>
2025-06-19 00:06:02 +0800

    Added Import Data for Table 5 for Multi Maxicare

--- src/app/api/protected/generate/exportT5/maxicare/route.ts
+++ src/app/api/protected/generate/exportT5/maxicare/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: NextRequest) {
 
   try {
     const initialData = await prisma.maxicare.groupBy({
-      by: ["Member_Type", "Diagnosis"],
+      by: ["Member_Type", "Diagnosis", "ICD_10_Code"],
       _sum: {
         Approved_Claim_Amount: true,
       },
@@ -88,29 +88,13 @@ export async function POST(req: NextRequest) {
       .flat()
       .map((data: any) => {
         const claimAmount = Math.round(data._sum.Approved_Claim_Amount ?? 0);
-        const claimAmount_percentage = Math.round(
+        const finalCA_percentage = Number(
           (claimAmount / totalClaimAmount) * 100
         );
-        const finalCA_percentage =
-          claimAmount_percentage === 0 && claimAmount !== 0
-            ? Number(
-                (
-                  Math.round((claimAmount / totalClaimAmount) * 100 * 100) / 100
-                ).toFixed(1)
-              )
-            : claimAmount_percentage;
+
         const claimCount = data._count.Approved_Claim_Amount;
-        const claimCount_percentage = Math.round(
-          (claimCount / totalClaimCount) * 100
-        );
-        const finalCC_percentage =
-          claimCount_percentage === 0 && claimCount !== 0
-            ? Number(
-                (
-                  Math.round((claimCount / totalClaimCount) * 100 * 100) / 100
-                ).toFixed(1)
-              )
-            : claimCount_percentage;
+        const finalCC_percentage = Number((claimCount / totalClaimCount) * 100);
+
         const averageClaimAmount = Math.round(
           data._avg.Approved_Claim_Amount ?? 0
         );
@@ -119,6 +103,7 @@ export async function POST(req: NextRequest) {
 
         return {
           ...payload,
+          ICD_10_Code: data.ICD_10_Code,
           claimAmount,
           claimAmount_percentage: finalCA_percentage,
           claimCount,
@@ -127,77 +112,6 @@ export async function POST(req: NextRequest) {
         };
       });
 
-    // get sum of all claimAmount and claimCount for all data
-    const totalApprovedClaimAmount = await prisma.maxicare.aggregate({
-      _sum: {
-        Approved_Claim_Amount: true,
-      },
-      _count: {
-        Approved_Claim_Amount: true,
-      },
-      where: {
-        Admission_Date: {
-          gte: startDate,
-          lte: endDate,
-        },
-        clientId,
-        Approved_Claim_Amount: {
-          not: null,
-        },
-      },
-    });
-
-    const { _sum, _count } = totalApprovedClaimAmount;
-    const totalClaimAmountSum = _sum.Approved_Claim_Amount ?? 0;
-    const totalClaimCountSum = _count.Approved_Claim_Amount ?? 0;
-
-    const memberTypes = Array.from(
-      new Set(processedData.map((d: any) => d.Member_Type))
-    );
-
-    const totals = memberTypes.map((type) => {
-      const rows = processedData.filter((d) => d.Member_Type === type);
-      const totalClaimAmount = rows.reduce((sum, r) => sum + r.claimAmount, 0);
-      const tCA_percentage = Math.round(
-        (totalClaimAmount / totalClaimAmountSum) * 100
-      );
-      const finalCA_percentage =
-        tCA_percentage === 0 && totalClaimAmount !== 0
-          ? Number(
-              (
-                Math.round(
-                  (totalClaimAmount / totalClaimAmountSum) * 100 * 100
-                ) / 100
-              ).toFixed(1)
-            )
-          : tCA_percentage;
-      const totalClaimCount = rows.reduce((sum, r) => sum + r.claimCount, 0);
-      const tCC_percentage = Math.round(
-        (totalClaimCount / totalClaimCountSum) * 100
-      );
-      const finalCC_percentage =
-        tCC_percentage === 0 && totalClaimCount !== 0
-          ? Number(
-              (
-                Math.round((totalClaimCount / totalClaimCountSum) * 100 * 100) /
-                100
-              ).toFixed(1)
-            )
-          : tCC_percentage;
-
-      return {
-        Member_Type: type,
-        claimAmount: totalClaimAmount,
-        claimCount: totalClaimCount,
-        claimAmount_percentage: finalCA_percentage,
-        claimCount_percentage: finalCC_percentage,
-        averageClaimAmount:
-          totalClaimCount > 0
-            ? Math.round(totalClaimAmount / totalClaimCount)
-            : 0,
-      };
-    });
-
     const payload = processedData.sort((a, b) => {
       const priorityOrder: Record<string, number> = { P: 1, D: 2 };
       const aPriority = priorityOrder[a.Member_Type] ?? 99;
@@ -205,9 +119,12 @@ export async function POST(req: NextRequest) {
       return aPriority - bPriority;
     });
 
+    console.log("Final Payload:", JSON.stringify(payload, null, 2));
+
     const rows = payload.map((item) => ({
       Member_Type: item.Member_Type,
       Diagnosis: item.Diagnosis,
+      ICD_10_Code: item.ICD_10_Code,
       claimAmount: item.claimAmount,
       claimAmount_percentage: item.claimAmount_percentage,
       claimCount: item.claimCount,
--- src/app/client/result/multi/page.tsx
+++ src/app/client/result/multi/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { useBRReportStore } from "@/lib/store";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Chart1, Chart2, Chart4 } from "./charts/charts";
 import { Table1, Table3, Table4, Table5, Table6 } from "./tables/tables";
 import { useRouter } from "next/navigation";
 import domtoimage from "dom-to-image-more";
 import JSZip from "jszip";
 import apiClient from "@/lib/axios";
+import { parse } from "papaparse";
 
 const Page = () => {
   const data = useBRReportStore((s) => s.data);
   const hasHydrated = useBRReportStore((s) => s.hasHydrated);
   const router = useRouter();
   const [show, setShow] = useState<"json" | "report" | "output">("report");
+  const [customIllnesses, setCustomIllnesses] = useState({
+    data: [],
+    totals: [],
+  });
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const charts = [
     "chart1-capture",
     "chart2-capture",
@@ -81,9 +87,26 @@ const Page = () => {
     link.click();
   };
 
+  const fetchCustomIllnesses = async () => {
+    const response = await apiClient.get(
+      `/generate/checkCustomIllnesses/maxicare?clientId=${data.lastData.clientId}&py=${data.lastData.py}`
+    );
+    if (response.data.success) {
+      console.log("Custom illnesses data:", response.data.data);
+      console.log("Custom illnesses totals:", response.data.totals);
+      setCustomIllnesses({
+        data: response.data.data,
+        totals: response.data.totals,
+      });
+    }
+  };
+
   useEffect(() => {
     if (hasHydrated && !data) {
       router.push("/");
+    } else {
+      if (!data?.lastData) return;
+      fetchCustomIllnesses();
     }
   }, [hasHydrated, data]);
 
@@ -92,6 +115,53 @@ const Page = () => {
   }
 
   if (!data) return null;
+
+  const handleImport = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: async (results) => {
+        const cleanedRows = results.data.map((row: any) => {
+          const {
+            Member_Type,
+            Diagnosis,
+            ICD_10_Code,
+            claimAmount,
+            claimAmount_percentage,
+            claimCount,
+            claimCount_percentage,
+            averageClaimAmount,
+          } = row;
+
+          return {
+            Member_Type: Member_Type?.trim(),
+            Diagnosis: Diagnosis?.trim(),
+            ICD_10_Code: ICD_10_Code?.trim(),
+            claimAmount: parseInt(claimAmount),
+            claimAmount_percentage: parseFloat(claimAmount_percentage),
+            claimCount: parseInt(claimCount),
+            claimCount_percentage: parseFloat(claimCount_percentage),
+            averageClaimAmount: parseInt(averageClaimAmount),
+          };
+        });
+
+        console.log("Parsed rows:", cleanedRows);
+
+        await apiClient.post("/generate/importT5/maxicare", {
+          clientId: data.lastData.clientId,
+          py: data.lastData.py,
+          rows: cleanedRows,
+        });
+
+        alert("Import complete");
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
+      },
+    });
+  };
 
   return (
     <div className="relative flex flex-col aptos-font p-4">
@@ -159,6 +229,13 @@ const Page = () => {
               </div>
               <div className="w-7/12 p-4">
                 <div className="w-1/2 flex justify-between mb-4">
+                  <input
+                    type="file"
+                    accept=".csv"
+                    onChange={handleImport}
+                    className="hidden"
+                    ref={fileInputRef}
+                  />
                   <button
                     className=" rounded-xl bg-green-400 px-2 py-1 hover:bg-green-500"
                     onClick={async () => {
@@ -191,13 +268,22 @@ const Page = () => {
                     {" "}
                     Export Data (CSV)
                   </button>
-                  <button className=" rounded-xl bg-blue-400 px-2 py-1 hover:bg-blue-500">
+                  <button
+                    className=" rounded-xl bg-blue-400 px-2 py-1 hover:bg-blue-500"
+                    onClick={() => fileInputRef.current?.click()}
+                  >
                     {" "}
                     Import Data (CSV)
                   </button>
                 </div>
-
-                <Table5 data={data.chart5.data} totals={data.chart5.total} />
+                {customIllnesses.data.length > 0 ? (
+                  <Table5
+                    data={customIllnesses.data}
+                    totals={customIllnesses.totals}
+                  />
+                ) : (
+                  <Table5 data={data.chart5.data} totals={data.chart5.total} />
+                )}
               </div>
               <div className="w-7/12 p-4">
                 <Table6 data={data.chart6.data} totals={data.chart6.total} />
