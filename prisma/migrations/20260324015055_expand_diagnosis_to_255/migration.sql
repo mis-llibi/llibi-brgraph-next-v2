f1c763057c0753cb7264951f9feacc74055a77b0
raizelybaam <95033305+raizelycid@users.noreply.github.com>
2026-03-25 12:32:50 +0800

    extend varchars

---
+++ prisma/migrations/20260324015055_expand_diagnosis_to_255/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `intellicare` MODIFY `Diagnosis` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `maxicare` MODIFY `Diagnosis` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `philcare` MODIFY `Diagnosis` VARCHAR(255) NULL;
---
+++ prisma/migrations/20260325043235_exntend_company/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE `intellicare` MODIFY `Company` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `intellicareMasterlist` MODIFY `COMPANY` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `maxicare` MODIFY `Company` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `maxicareMasterlist` MODIFY `COMPANY` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `philcare` MODIFY `Company` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `philcareMasterlist` MODIFY `SUB_OFFICE_NAME` VARCHAR(255) NULL;
